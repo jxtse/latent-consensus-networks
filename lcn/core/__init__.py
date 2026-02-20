dfa356281dbb926336f1581eff8056ba48be4c29
Jinxiang Xie (FESCO Adecco Human Resources) <v-jinxixie@microsoft.com>
2026-02-20 10:15:18 +0800

    chore(core): update core module exports

--- lcn/core/__init__.py
+++ lcn/core/__init__.py
@@ -1,1 +1,14 @@
 """Core components for Latent Consensus Networks."""
+
+from lcn.core.agent import LCNAgent
+from lcn.core.kv_cache import HierarchicalKVCache, KVCache
+from lcn.core.attention import CrossLevelAttention
+from lcn.core.consensus import ConsensusProtocol
+
+__all__ = [
+    "LCNAgent",
+    "HierarchicalKVCache",
+    "KVCache",
+    "CrossLevelAttention",
+    "ConsensusProtocol",
+]
